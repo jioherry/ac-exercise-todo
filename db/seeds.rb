81422b0bc0d2e44410e569e1f47bf89898fcc8b9
Ray <ray@Rayde-MacBook-Pro.local>
2017-03-09 11:26:43 +0800

    完成首頁

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Todo.destroy_all
+
+todo_list = [
+	{
+		"title" => "Ray's Party", "date" => "2017-03-01", "description" => "This is my party"
+	}
+]
+todo_list.each do |todo|
+
+	Todo.create(
+
+		:title => todo["title"],
+		:date => todo["date"],
+		:description => todo["description"],
+
+	)
+end
